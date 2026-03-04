acb5d8ee8b17fc7690b8c1687626d2c04c359b26
meronoumer <meron.oumer2005@gmail.com>
2026-03-03 21:48:24 -0500

    fininshed website

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleStats() {
+    const box = document.getElementById("statsBox");
+
+    if (box.style.display === "none") {
+        box.style.display = "block";
+    } else {
+        box.style.display = "none";
+    }
+}
